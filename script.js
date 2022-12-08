71d511e18d2091ca38360f09d94e3b127090246a
benjaminmahmutovic <104673432+benjaminmahmutovic@users.noreply.github.com>
2022-12-08 11:35:48 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function prvaSlika() {
+    let slika = document.querySelector(".slikica"); 
+    slika.src="slika1.jpg"; 
+    }
+    function drugaSlika() {
+        let slika = document.querySelector(".slikica"); 
+        slika.src="slika2.jpg"; 
+        }
+        function trecaSlika() {
+            let slika = document.querySelector(".slikica"); 
+            slika.src="slika3.jpg"; 
+            }
+            
